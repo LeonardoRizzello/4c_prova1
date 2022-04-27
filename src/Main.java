4cc62e56cd2d0b1de28cc38f2800bccd5beda880
rizzello <rizzello@hotmail.com>
2022-04-27 11:46:27 +0200

    versione 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+ * 
+ * @author Samuele Panico
+ * @version 1.0
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.out.println("Progetto di prova GitHub");
+	}
+
+}
