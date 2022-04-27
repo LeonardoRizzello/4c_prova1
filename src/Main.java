469b8f8554837309e866e33a13e06a4e8dd56939
LeonardoRizzello <103572267+LeonardoRizzello@users.noreply.github.com>
2022-04-27 12:01:46 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
  * 
  * @author Rizzello Leonardo
  * 
- * @version 2.0
+ * @version 2b online
  */
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("prova 2");
+		System.out.println("prova 2b online");
 	}
 
 }
